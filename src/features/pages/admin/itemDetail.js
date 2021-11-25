1f52f0843e8509c004858f5ca37dd9c878a2a43c
1997808 <khanhhuyle22@gmail.com>
2021-11-25 16:02:51 +0700

    dummy pages

---
+++ src/features/pages/admin/itemDetail.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ItemDetail() {
+  return (
+    <div className="">
+      <p>404</p>
+    </div>
+  );
+}
---
+++ src/features/pages/client/login.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Login() {
+  return (
+    <div className="">
+      <p>404</p>
+    </div>
+  );
+}
