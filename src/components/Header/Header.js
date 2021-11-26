787cd1ac28553d2124e602560ad937e0d0f575d7
1997808 <khanhhuyle22@gmail.com>
2021-11-26 10:05:46 +0700

    setup tailwind

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,17 @@ export const Header = ({ title }) => {
   return (
     <>
       <h1>Header</h1>
-      <h1>{title}</h1>
+      <div className="btn-group" role="group" aria-label="Basic example">
+        <button type="button" className="btn btn-primary">
+          Left
+        </button>
+        <button type="button" className="btn btn-primary">
+          Middle
+        </button>
+        <button type="button" className="btn btn-primary">
+          Right
+        </button>
+      </div>
     </>
   );
 };
--- src/features/pages/client/index.js
+++ src/features/pages/client/index.js
@@ -1,9 +1,19 @@
 import React from "react";
+import logo from "../../../assets/logo.png";
 
 export const Home = () => {
   return (
-    <div className="">
-      <p>404</p>
+    // <div className="">
+    //   <p>404</p>
+    <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+      <div className="flex-shrink-0">
+        <img className="h-12 w-12" src={logo} alt="ChitChat Logo" />
+      </div>
+      <div>
+        <div className="text-xl font-medium text-black">ChitChat</div>
+        <p className="text-gray-500">You have a new message!</p>
+      </div>
     </div>
+    // </div>
   );
 };
